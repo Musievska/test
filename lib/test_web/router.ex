3cd51cfc726dd3ff392b1b27cdecdc2229ac7209
Musievska <dilyana.musievska@gmail.com>
2023-07-09 11:44:12 +0000

    plug

--- lib/test_web/router.ex
+++ lib/test_web/router.ex
@@ -8,6 +8,7 @@ defmodule TestWeb.Router do
     plug :put_root_layout, html: {TestWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug TestWeb.Plugs.Locale, "en"
   end
 
   pipeline :api do
